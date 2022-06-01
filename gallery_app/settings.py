ad542d7f67baf1a73ceba0b5ded07868be1338ce
Karenkoech <karenkoech3@gmail.com>
2022-06-01 10:42:57 -0400

    deployment updates

--- gallery_app/settings.py
+++ gallery_app/settings.py
@@ -90,7 +90,7 @@ ROOT_URLCONF = 'gallery_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
