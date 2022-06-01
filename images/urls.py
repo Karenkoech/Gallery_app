88eb954b34a89c3de550eda5a205c09a0554404b
Karenkoech <karenkoech3@gmail.com>
2022-06-01 16:42:57 -0400

    configure urls.py to register the MEDIA_ROOT route

--- images/urls.py
+++ images/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -10,3 +12,7 @@ urlpatterns = [
     path('search_category/', views.search_category, name='search_category'),
     path('search_location/', views.search_location, name='search_location'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
